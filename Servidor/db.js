d8f612f74a15b517ad6a20822ca0dd25bb335a4e
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-06-10 16:43:07 -0600

    cambios

--- Servidor/db.js
+++ Servidor/db.js
@@ -1,6 +1,7 @@
-import { Pool } from 'pg'
+import pg from 'pg'
 
-const pool = new Pool({
+
+const pool = new pg.Pool({
     user: 'aewmoyxm',
     password: 'FuiR_MsqIhRN3jSlGF6xS5nLjN6-dAhS',
     host: 'ziggy.db.elephantsql.com',
@@ -8,3 +9,5 @@ const pool = new Pool({
     database: 'aewmoyxm',
     max: 10
 })
+
+export default pool
--- Servidor/index.js
+++ Servidor/index.js
@@ -5,11 +5,37 @@ import morgan from 'morgan'
 import pg from 'pg'
 const PORT = process.env.PORT || 3000
 const app = express() // Instancia de express.
+import pool from './db.js'
+
 
 app.use(cors()) // Configuración de CORS.
 app.use(express.json()) // Configuración de body-parser.
 
+// Rutes
 
+// Creando un todo.
+
+// El async hace el request más fácil.
+app.post('/todos', async (req, res) => {
+    try {
+        
+        const { description } = req.body
+        const newTodo = await pool.query('INSERT INTO todos (description) VALUES ($1)', [description]) 
+        // todos es la tabla. description es la columna. $1 es el valor. [description] es lo que se va a insertar.
+        res.json(newTodo)
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+// Obteniendo todos los todos.
+
+// Obteniendo un todo.
+
+// Actualizando un todo.
+
+// Eliminando un todo.
 
 // Iniciamos el servidor.
 app.listen(PORT, () => {
