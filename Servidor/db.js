632e42600af816272738a5a9116d1d39291d9219
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-06-15 17:22:08 -0600

    agregando el dotenv al proyecto

--- Servidor/db.js
+++ Servidor/db.js
@@ -1,13 +1,13 @@
 import pg from 'pg'
-
+import 'dotenv/config'
 
 const pool = new pg.Pool({
-    user: 'aewmoyxm',
-    password: 'FuiR_MsqIhRN3jSlGF6xS5nLjN6-dAhS',
-    host: 'ziggy.db.elephantsql.com',
-    port: 5432,
-    database: 'aewmoyxm',
-    max: 10
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
+    max: process.env.MAX
 })
 
 export default pool
