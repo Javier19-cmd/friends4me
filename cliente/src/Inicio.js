49f228c32e7b4be4073739fbff41f5f60845fcef
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-06-19 21:44:45 -0600

    agregando link para el diagrama entidad relación

--- cliente/src/Inicio.js
+++ cliente/src/Inicio.js
@@ -1,3 +1,7 @@
+/**
+ * Diagrama entidad-relación: https://lucid.app/lucidchart/696bb8b0-bf97-4d0b-bebb-6636d2584710/edit?beaconFlowId=D7F5FADF353A9503&invitationId=inv_0932a71c-933a-4e7e-8a6f-13e365ef57cf&page=0_0#
+*/
+
 import React from 'react'
 import './inicio.css'
 
